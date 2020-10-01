d3bf3f398902f6c793d3ac09c693c068a19b10ff
Reysmer Valle <reysmerwvr@gmail.com>
2020-09-30 23:44:26 -0500

    Fix card_page file typo

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:components/src/pages/alert_page.dart';
 import 'package:components/src/pages/avatar_page.dart';
 import 'package:components/src/pages/home_page.dart';
-import 'package:components/src/pages/cart_page.dart';
+import 'package:components/src/pages/card_page.dart';
 import 'package:components/src/pages/animated_container_page.dart';
 import 'package:components/src/pages/input_page.dart';
 import 'package:components/src/pages/slider_page.dart';
